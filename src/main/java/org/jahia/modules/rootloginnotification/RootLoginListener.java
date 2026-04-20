99ee6c949a7c0f06d484d6052ac8091c11ff3114
Florent BOURASSE <fbourasse@jahia.com>
2026-04-20 14:30:42 +0200

    Getting rid off Spring framework library to catch correctly the login event
    Signed-off-by: Florent BOURASSE <fbourasse@jahia.com>

--- src/main/java/org/jahia/modules/rootloginnotification/RootLoginListener.java
+++ src/main/java/org/jahia/modules/rootloginnotification/RootLoginListener.java
@@ -1,31 +1,35 @@
 package org.jahia.modules.rootloginnotification;
 
+import org.apache.commons.lang.StringUtils;
+import org.jahia.params.valves.BaseLoginEvent;
+import org.jahia.services.mail.MailService;
+import org.jahia.services.observation.JahiaEventListener;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+import javax.servlet.http.HttpServletRequest;
 import java.time.Instant;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
-import javax.servlet.http.HttpServletRequest;
-import org.apache.commons.lang.StringUtils;
-import org.jahia.params.valves.LoginEngineAuthValveImpl.LoginEvent;
-import org.jahia.services.mail.MailService;
-import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
-import org.springframework.context.ApplicationListener;
+import java.util.EventObject;
 
-@Component(immediate = true, service = ApplicationListener.class)
-public final class RootLoginListener implements ApplicationListener<LoginEvent> {
+@Component(immediate = true, service = JahiaEventListener.class)
+public final class RootLoginListener implements JahiaEventListener<BaseLoginEvent> {
 
     private static final String REMOTE_ADDRESS_HEADER = "x-forwarded-for";
     private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter
             .ofPattern("yyyy/MM/dd 'at' HH:mm:ss z")
             .withZone(ZoneId.systemDefault());
+    private static final Class<BaseLoginEvent>[] ALLOWED_EVENT_TYPES = new Class[]{BaseLoginEvent.class};
 
     private MailService mailService;
     private RootLoginNotificationConfig config;
 
     @Override
-    public void onApplicationEvent(LoginEvent loginEvent) {
-        if (mailService.isEnabled() && loginEvent.getJahiaUser().isRoot()) {
-            final HttpServletRequest request = loginEvent.getAuthValveContext().getRequest();
+    public void onEvent(EventObject event) {
+        final BaseLoginEvent baseLoginEvent = (BaseLoginEvent) event;
+        if (mailService.isEnabled() && baseLoginEvent.getJahiaUser().isRoot()) {
+            final HttpServletRequest request = baseLoginEvent.getAuthValveContext().getRequest();
             String remoteAddress = request.getHeader(REMOTE_ADDRESS_HEADER);
             if (remoteAddress == null) {
                 remoteAddress = request.getRemoteAddr();
@@ -36,7 +40,7 @@ public final class RootLoginListener implements ApplicationListener<LoginEvent> 
                 site = "systemsite";
             }
             final String serverName = request.getServerName();
-            final String loginTime = DATE_FORMATTER.format(Instant.ofEpochMilli(loginEvent.getTimestamp()));
+            final String loginTime = DATE_FORMATTER.format(Instant.ofEpochMilli(baseLoginEvent.getTimestamp()));
 
             final String sender = StringUtils.defaultIfEmpty(config.getSender(), mailService.defaultSender());
             final String recipient = StringUtils.defaultIfEmpty(config.getRecipient(), mailService.defaultRecipient());
@@ -49,6 +53,12 @@ public final class RootLoginListener implements ApplicationListener<LoginEvent> 
 
             mailService.sendMessage(sender, recipient, null, null, subject, null, body);
         }
+    }
+
+
+    @Override
+    public Class<BaseLoginEvent>[] getEventTypes() {
+        return ALLOWED_EVENT_TYPES;
     }
 
     @Reference
